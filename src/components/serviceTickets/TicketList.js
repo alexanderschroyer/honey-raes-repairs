b6e71a16be4c78e82b29f218af47edd76fcef938
Alexander Schroyer <alexanderschroyer@gmail.com>
2021-09-03 13:15:54 -0500

    refrshes ticketList upon ticket deletion

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -18,44 +18,43 @@ export const TicketList = () => {
         []
     )
 
-    useEffect(() => {
-        const activateTicketCount = tickets.filter(ticket => ticket.dateCompleted === "").length
-        setActive(`There are ${activateTicketCount} open tickets`)
-    }, [tickets])
-
-
     const deleteTicket = (id) => {
         fetch(`http://localhost:8088/serviceTickets/${id}`, {
             method: "DELETE"
         })
-            .then(() => {
-                fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
-                    .then(res => res.json())
-                    .then((data) => {
-                        updateTickets(data)
-                    })
-            }
+        .then(() => {
+            fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
+                .then(res => res.json())
+                .then((data) => {
+                    updateTickets(data)
+                })
+        })
+    }
 
+        useEffect(() => {
+            const activateTicketCount = tickets.filter(ticket => ticket.dateCompleted === "").length
+            setActive(`There are ${activateTicketCount} open tickets`)
+        }, [tickets])
         
     return (
-            <>
-                <div>
-                    <button onClick={() => history.push("/tickets/create")}>Create Ticket</button>
-                </div>
-                {active}
-                {
-                    tickets.map(
-                        (ticket) => {
-                            return <div key={`ticket--${ticket.id}`}>
-                                <p className={`ticket ${ticket.emergency ? 'emergency' : ''}`}>
-                                    {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}> {ticket.description} </Link>
-                                    submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
-                                    <button onClick={() => { deleteTicket(ticket.id) }}>Delete</button>
-                                </p>
-                            </div>
-                        }
-                    )
-                }
-            </>
-        )
-    }
+        <>
+            <div>
+                <button onClick={() => history.push("/tickets/create")}>Create Ticket</button>
+            </div>
+            {active}
+            {
+                tickets.map(
+                    (ticket) => {
+                        return <div key={`ticket--${ticket.id}`}> 
+                        <p className={`ticket ${ticket.emergency ? 'emergency' : ''}`}>
+                            {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}> {ticket.description} </Link> 
+                            submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
+                            <button onClick={() => {deleteTicket(ticket.id)}}>Delete</button>
+                            </p>
+                        </div>
+                    }
+                )
+            }
+        </>
+    )
+}
